d57170b17d3aed97f2c7b85bb1b2b78cbfc090a2
Fluxusfrequency <bennlewis@gmail.com>
2013-08-26 21:03:51 -0600

    Generated initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactManager::Application.config.secret_token = 'efcc4d8ca4e116bcad059e13dd0d7198f34080ddb142b7db6476ea48546e97c34ab863c5749179c6949e2eda3aa247856d5648e8095295e1650cd2d084092745'
