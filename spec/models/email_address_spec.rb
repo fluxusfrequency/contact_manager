45a2400bb77404b4af803a5dc7ad86939644caaa
Fluxusfrequency <bennlewis@gmail.com>
2013-08-29 22:04:48 -0600

    Added validity specs to email

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) do
+    EmailAddress.new(address: "johndoe@example.com", person_id: 1)
+  end
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
 end
