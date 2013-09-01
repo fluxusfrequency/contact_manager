4d0a0d02a55bbe3b3cfd62ff50f555e51ee743f3
Fluxusfrequency <bennlewis@gmail.com>
2013-09-01 13:28:13 -0600

    finished creating users

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,10 +7,15 @@ describe SessionsController do
     before(:each) do
       Rails.application.routes.draw do
         resource :sessions, :only => [:create]
+        root to: 'site#index'
       end
     end
 
-    it "creates a user from twitter data" do
+    after(:each) do
+      Rails.application.reload_routes!
+    end
+
+    it "logs in a new user" do
       @request.env["omniauth.auth"] = {
         'provider' => 'twitter',
         'info' => {'name' => 'Alice Smith'},
@@ -19,19 +24,31 @@ describe SessionsController do
 
       post :create
       user = User.find_by_uid_and_provider('abc123', 'twitter')
-      expect(user.name).to eq("Alice Smith")
+      expect(controller.current_user.id).to eq(user.id)
     end
 
-    it "doesn't create duplicate users" do
+    it "logs in an existing user" do
       @request.env["omniauth.auth"] = {
         'provider' => 'twitter',
         'info' => {'name' => 'Bob Jones'},
         'uid' => 'xyz456'
       }
-      User.create(provider: 'twitter', uid: 'xyz456', name: 'Bob Jones')
+      user = User.create(provider: 'twitter', uid: 'xyz456', name: 'Bob Jones')
 
       post :create
       expect(User.count).to eq(1)
+      expect(controller.current_user.id).to eq(user.id)
+    end
+
+    it 'redirects to the companies page' do
+      @request.env["omniauth.auth"] = {
+        'provider' => 'twitter',
+        'info' => {'name' => 'Charlie Allen'},
+        'uid' => 'prq987'
+      }
+      user = User.create(provider: 'twitter', uid: 'prq987', name: 'Charlie Allen')
+      post :create
+      expect(response).to redirect_to(root_path)
     end
 
   end
