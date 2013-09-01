e8743ed76ff03507700458fec5dfe3e08d2193ff
Fluxusfrequency <bennlewis@gmail.com>
2013-09-01 13:42:09 -0600

    Added loginlogout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 ContactManager::Application.routes.draw do
+  root to: 'site#index'
+
   resources :companies
-
-
   resources :email_addresses, :except => [:index, :show]
-
-
   resources :phone_numbers, :except => [:index, :show]
-
-
   resources :people
 
   get '/auth/:provider/callback' => 'sessions#create'
+  match "/login" => redirect("/auth/twitter"), as: :login
+  delete "/logout" => "sessions#destroy", as: :logout
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
